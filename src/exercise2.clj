057af3b2c3ecac60cae7558278fd255df03710bb
nicolas chirichella <nicochirichella_c93@hotmail.com>
2017-08-24 08:56:38 -0300

    refactor de ejercicio 2

--- src/exercise2.clj
+++ src/exercise2.clj
@@ -1,14 +1,14 @@
 (ns exercise2)
 
 (defn es-mayor-a-cinco [numero]
-	(if (> numero 5)
-		true
-		false))
+	(> numero 5)
+)
 
 (defn only-greater-than-five 
 "Toma una lista inicial y devuelve una con los elementos mayores a 5"
 		[listaNumero]
-	(filter es-mayor-a-cinco listaNumero))
+	(filter es-mayor-a-cinco listaNumero)
+)
 	
 
 	
